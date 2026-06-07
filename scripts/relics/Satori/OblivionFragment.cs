135f2bee1fb8223135e6d34e11b4bd85f8a9d58d
Destroyer <2673630208@qq.com>
2026-06-08 02:53:56 +0800

    refactor: 遗忘残片——不再获取已有遗物并排除 LavaRock

--- scripts/relics/Satori/OblivionFragment.cs
+++ scripts/relics/Satori/OblivionFragment.cs
@@ -10,6 +10,7 @@ using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Events;
 using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.Models.Relics;
 using MegaCrit.Sts2.Core.Rewards;
 
 namespace TouhouAncients.Scripts.relics;
@@ -17,7 +18,7 @@ namespace TouhouAncients.Scripts.relics;
 /// <summary>
 /// 遗忘残片：获得随机三个涅奥的初始遗物。
 /// </summary>
-[Pool(typeof(SharedRelicPool))]
+[Pool(typeof(EventRelicPool))]
 public class OblivionFragment : TouhouAncientRelics
 {
     protected override IEnumerable<DynamicVar> CanonicalVars => [new DynamicVar("Relics", 3)];
@@ -30,16 +31,23 @@ public class OblivionFragment : TouhouAncientRelics
 
         // 获取所有涅奥初始遗物，排除自身
         var neowRelics = ModelDb.Event<Neow>().AllPossibleOptions
-            .Where(o => o.Relic != null && o.Relic.IsAllowed(state) && !(o.Relic is OblivionFragment))
+            .Where(o => o.Relic != null && o.Relic.IsAllowed(state) && o.Relic is not OblivionFragment or LavaRock &&
+                        Owner.Relics.All(x => x.Id.Entry != o.Relic.Id.Entry))
             .Select(o => o.Relic).OfType<RelicModel>()
             .ToList();
 
+        if (neowRelics.Count <= 0) return;
+        
         // 随机取3个
-        var chosen = neowRelics.UnstableShuffle(base.Owner.PlayerRng.Rewards).Take(DynamicVars["Relics"].IntValue).ToList();
+        var chosen = neowRelics.UnstableShuffle(base.Owner.PlayerRng.Rewards).Take(DynamicVars["Relics"].IntValue)
+            .ToList();
 
-        // 生成奖励
-        var rewards = chosen.Select(r => new RelicReward(r, base.Owner)).ToList<Reward>();
-        await new RewardsSet(base.Owner).WithCustomRewards(rewards).Offer();
-        base.Status = RelicStatus.Disabled;
+        if (chosen.Count > 0)
+        {
+            // 生成奖励
+            var rewards = chosen.Select(r => new RelicReward(r, base.Owner)).ToList<Reward>();
+            await new RewardsSet(base.Owner).WithCustomRewards(rewards).Offer();
+            base.Status = RelicStatus.Disabled;
+        }
     }
 }
