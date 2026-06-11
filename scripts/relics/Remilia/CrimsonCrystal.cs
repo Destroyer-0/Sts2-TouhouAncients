d634e2376e636260a6f91d96b21a22258e167671
Destroyer <2673630208@qq.com>
2026-06-12 00:57:54 +0800

    refactor: 绯色水晶——添加战斗生命周期状态管理

--- scripts/relics/Remilia/CrimsonCrystal.cs
+++ scripts/relics/Remilia/CrimsonCrystal.cs
@@ -1,12 +1,16 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.Rooms;
 
 namespace TouhouAncients.Scripts.relics;
 
@@ -23,6 +27,36 @@ public class CrimsonCrystal : TouhouAncientRelics
         new EnergyVar(1)
     ];
 
+    
+    public override Task BeforeCombatStart()
+    {
+        if (CanGetEnergy())
+        {
+            base.Status = RelicStatus.Active;
+        }
+        return Task.CompletedTask;
+    }
+
+    public override Task AfterCurrentHpChanged(Creature creature, decimal delta)
+    {
+        if (creature != base.Owner.Creature)
+        {
+            return Task.CompletedTask;
+        }
+        if (!CombatManager.Instance.IsInProgress)
+        {
+            return Task.CompletedTask;
+        }
+        base.Status = (CanGetEnergy() ? RelicStatus.Active : RelicStatus.Normal);
+        return Task.CompletedTask;
+    }
+
+    public override Task AfterCombatEnd(CombatRoom room)
+    {
+        base.Status =  RelicStatus.Normal;
+        return base.AfterCombatEnd(room);
+    }
+
     protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.ForEnergy(this)];
 
     public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
@@ -32,11 +66,12 @@ public class CrimsonCrystal : TouhouAncientRelics
         var creature = player.Creature;
         if (creature.MaxHp <= 0) return;
 
-        decimal hpPercent = creature.CurrentHp * 100m / creature.MaxHp;
-        if (hpPercent >= base.DynamicVars["HpThreshold"].BaseValue)
+        if (CanGetEnergy())
         {
             Flash();
             await PlayerCmd.GainEnergy(base.DynamicVars.Energy.IntValue, player);
         }
     }
+
+    private bool CanGetEnergy() => Owner.Creature.CurrentHp * 100m / Owner.Creature.MaxHp>= base.DynamicVars["HpThreshold"].BaseValue;
 }
