39411a868b7632203b9e4c81b73e371fc06009f1
Destroyer <2673630208@qq.com>
2026-06-16 12:02:34 +0800

    refactor: 吞天之勺——改用附魔实现持久化

--- scripts/relics/Yuuma/SkySwallowingSpoon.cs
+++ scripts/relics/Yuuma/SkySwallowingSpoon.cs
@@ -1,18 +1,19 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
 using BaseLib.Utils;
-using HarmonyLib;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using MegaCrit.Sts2.Core.Logging;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
 using MegaCrit.Sts2.Core.Nodes.Vfx;
+using MegaCrit.Sts2.Core.Rooms;
 using MegaCrit.Sts2.Core.Saves.Runs;
+using TouhouAncients.Scripts.Enchantment;
 
 namespace TouhouAncients.Scripts.relics;
 
@@ -24,6 +25,7 @@ namespace TouhouAncients.Scripts.relics;
 public class SkySwallowingSpoon : TouhouAncientRelics
 {
     public override string DefaultFileName => "yuuma_default";
+
     [SavedProperty]
     public int TouhouAncients_SwallowedCards
     {
@@ -42,17 +44,45 @@ public class SkySwallowingSpoon : TouhouAncientRelics
     public override bool ShowCounter => true;
     public override int DisplayAmount => TouhouAncients_SwallowedCards;
 
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new MaxHpVar(8)];
-    
-    
-    // 阻止诅咒加入牌组
-    public override bool ShouldAddToDeck(CardModel card)
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new MaxHpVar(5)];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.Static(StaticHoverTip.ReplayStatic),
+    ];
+
+    public override async Task AfterCardChangedPiles(CardModel card, PileType oldPileType, AbstractModel? source)
     {
-        if (card.Owner != base.Owner) return true;
-        TouhouAncients_SwallowedCards++;
-        Flash();
-        CreatureCmd.GainMaxHp(card.Owner.Creature, base.DynamicVars["MaxHp"].IntValue);
-        return false;
-        return true;
+        if (base.Owner.Creature.IsDead || card.Owner != base.Owner)
+        {
+            return;
+        }
+        CardPile? pile = card.Pile;
+        if (pile != null && pile.Type == PileType.Deck && card.Enchantment == null)
+        {
+            Flash();
+            CardCmd.Enchant<BloodPond>(card, 1m);
+        }
+    }
+
+    public override async Task AfterCombatEnd(CombatRoom room)
+    {
+        var list = Owner.Deck.Cards
+            .Where(c => c.Enchantment is BloodPond)
+            .ToList();
+        if (list.Count > 0)
+        {
+            Flash();
+            foreach (var cardModel in list)
+            {
+                TouhouAncients_SwallowedCards++;
+                await CardPileCmd.RemoveFromDeck(cardModel);
+                await CreatureCmd.GainMaxHp(Owner.Creature,
+                    cardModel.Type == CardType.Curse ? 2 : 1 * base.DynamicVars["MaxHp"].IntValue);
+            }
+            NCombatRoom.Instance?.GetCreatureNode(base.Owner.Creature)
+                ?.ScaleTo(MathF.Min(2.5f, 1 + TouhouAncients_SwallowedCards * 0.05f), 0f);
+        }
+        return;
     }
 }
