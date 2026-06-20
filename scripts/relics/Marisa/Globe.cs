cffc995d0b8647920abf4acacb637d06ec99737b
Destroyer <2673630208@qq.com>
2026-06-21 02:26:03 +0800

    feat: 太阳系仪添加 Transform 悬浮提示

--- scripts/relics/Marisa/Globe.cs
+++ scripts/relics/Marisa/Globe.cs
@@ -6,6 +6,7 @@ using MegaCrit.Sts2.Core.CardSelection;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Factories;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.RelicPools;
@@ -20,19 +21,24 @@ public class Globe : TouhouAncientRelics
 {
     protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(5)];
 
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(StaticHoverTip.Transform)];
     public override bool HasUponPickupEffect => true;
 
     public override async Task AfterObtained()
     {
-        List<CardModel> list = (await CardSelectCmd.FromDeckForTransformation(prefs: new CardSelectorPrefs(CardSelectorPrefs.TransformSelectionPrompt, base.DynamicVars.Cards.IntValue), player: base.Owner)).ToList();
+        List<CardModel> list = (await CardSelectCmd.FromDeckForTransformation(
+            prefs: new CardSelectorPrefs(CardSelectorPrefs.TransformSelectionPrompt, base.DynamicVars.Cards.IntValue),
+            player: base.Owner)).ToList();
         foreach (CardModel item in list)
         {
             var shouldUpgrade = item.IsUpgraded;
-            CardModel cardModel = CardFactory.CreateRandomCardForTransform(item, isInCombat: false, base.Owner.PlayerRng.Transformations);
+            CardModel cardModel =
+                CardFactory.CreateRandomCardForTransform(item, isInCombat: false, base.Owner.PlayerRng.Transformations);
             if (shouldUpgrade)
             {
                 CardCmd.Upgrade(cardModel);
             }
+
             await CardCmd.Transform(item, cardModel);
         }
     }
