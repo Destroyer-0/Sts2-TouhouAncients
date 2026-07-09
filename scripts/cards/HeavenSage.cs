5d2235fd1df665f3b9543918b93148acc42ec5f6
Destroyer <2673630208@qq.com>
2026-07-10 07:17:23 +0800

    refactor: 废弃卡牌「天堂智者」
    整文件注释掉，该卡牌不再使用

--- scripts/cards/HeavenSage.cs
+++ scripts/cards/HeavenSage.cs
@@ -1,34 +1,34 @@
-﻿using BaseLib.Utils;
-using MegaCrit.Sts2.Core.Commands;
-using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.Models.CardPools;
-using MegaCrit.Sts2.Core.Models.Powers;
-using TouhouAncients.Scripts.powers;
-
-namespace TouhouAncients.Scripts.cards;
-
-[Pool(typeof(ColorlessCardPool))]
-public class HeavenSage : TouhouAncientCards
-{
-    private const int energyCost = 2;
-    private const CardType type = CardType.Power;
-    private const CardRarity rarity = CardRarity.Uncommon;
-    private const TargetType targetType = TargetType.None;
-    private const bool shouldShowInCardLibrary = true;
-    
-    public HeavenSage() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary)
-    {
-    }
-
-    protected override void OnUpgrade()
-    {
-        EnergyCost.UpgradeBy(-1);
-    }
-    
-    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
-    {
-        await PowerCmd.Apply<HeavenSagePower>(choiceContext, base.Owner.Creature, 1m, base.Owner.Creature, this);
-    }
-
-}
+﻿// using BaseLib.Utils;
+// using MegaCrit.Sts2.Core.Commands;
+// using MegaCrit.Sts2.Core.Entities.Cards;
+// using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+// using MegaCrit.Sts2.Core.Models.CardPools;
+// using MegaCrit.Sts2.Core.Models.Powers;
+// using TouhouAncients.Scripts.powers;
+//
+// namespace TouhouAncients.Scripts.cards;
+//
+// [Pool(typeof(ColorlessCardPool))]
+// public class HeavenSage : TouhouAncientCards
+// {
+//     private const int energyCost = 2;
+//     private const CardType type = CardType.Power;
+//     private const CardRarity rarity = CardRarity.Uncommon;
+//     private const TargetType targetType = TargetType.None;
+//     private const bool shouldShowInCardLibrary = true;
+//     
+//     public HeavenSage() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary)
+//     {
+//     }
+//
+//     protected override void OnUpgrade()
+//     {
+//         EnergyCost.UpgradeBy(-1);
+//     }
+//     
+//     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+//     {
+//         await PowerCmd.Apply<HeavenSagePower>(choiceContext, base.Owner.Creature, 1m, base.Owner.Creature, this);
+//     }
+//
+// }
